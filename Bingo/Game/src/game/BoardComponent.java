eaa242307cd82337c5c35a7c1324294e199f0277
twoood <tpw0007@uah.edu>
2018-04-06 12:24:53 -0500

    Chips and Board

---
+++ Bingo/Game/src/game/BoardComponent.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+import java.util.*;
+
+/**
+ *
+ * @author dale
+ */
+public class BoardComponent extends JComponent {
+    
+}
---
+++ Bingo/Game/src/game/testBoardChips.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+/**
+ *
+ * @author dale
+ */
+public class testBoardChips {
+    public static void main(String[] args)
+   {
+  
+   }
+}
