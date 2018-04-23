7cf02caa014a38f5ec7903679aef9f9d0913a0ff
Sara Bonds <38139449+livelyk8@users.noreply.github.com>
2018-04-23 16:16:43 -0500

    Add files via upload

---
+++ Bingo/Game/src/game/BingoFact.java
@@ -0,0 +1,50 @@
+/*
+ * Called when a BINGO is successfully called, rewards winner with a BINGO Fact!
+ */
+package game;
+import java.util.Random;
+import java.util.Vector;
+/**
+ * Author: Sara Lively
+ * A-Number: A25227626
+ * This is a class is for when a player successfully calls BINGO. they are rewarded with a BINGO Fact!
+ */
+public class BingoFact 
+{
+
+    private Vector<String> BingoFacts = new Vector<String>(10, 2);
+    private String Fact;
+    private final String FactOne = "Bingo cards were first used by the Germans in 1800s as an educational tool designed to help the students learn multiplication tables easily.";
+    private final String FactTwo = "Bingo cards in UK are also called 'Housie'.";
+    private final String FactThree = "US bingo cards have 5x5 squares while UK bingo cards have 3 rows and 9 columns.";
+    private final String FactFour = "In a standard bingo card, a player can come up with 1,474,200 different winning combinations.";
+    private final String FactFive = "Bingo was introduced way back in the 1930's and back then they played around 10,000 games of bingo every day throughouth the Us, UK, and rest of the world.";
+    private final String FactSix = "How long does the average bingo game last for? Usually, bingo games last for about 3-6 minutes. That means you have a shot of hitting the jackpot every 5 minutes roughly.";
+    
+    /**
+     * This creates an instance of BingoFact which can display a fact to the user after
+     * winning a game of BINGO.
+     */
+
+    public BingoFact()
+    {
+        BingoFacts.add(FactOne);
+        BingoFacts.add(FactTwo);
+        BingoFacts.add(FactThree);
+        BingoFacts.add(FactFour);
+        BingoFacts.add(FactFive);
+        BingoFacts.add(FactSix);
+    }
+
+    /**
+     *This will display the BINGO Fact. 
+     */
+
+    public void DisplayFact()
+    {
+        int rnd = new Random().nextInt(BingoFacts.size());
+        Fact = BingoFacts.get(rnd);
+        BingoFactGUI Gui = new BingoFactGUI(Fact);
+    }
+    
+}
---
+++ Bingo/Game/src/game/BingoFactGUI.java
@@ -0,0 +1,37 @@
+/*
+ * GUI interface to properly display the Bingo Fact in a window.
+ */
+
+/**
+ * Author: Sara Lively
+ * A-Number: A25227626
+ * This is a class for making the BINGO Fact GUI.
+ */
+package game;
+import java.awt.*;
+import javax.swing.*;
+
+
+public class BingoFactGUI
+{
+    private final JFrame Frame = new JFrame();
+    private final JButton Ok = new JButton("Ok");
+    private final JTextField WinnerText = new JTextField(50);
+    private final JTextField FactText = new JTextField(50);
+    /**
+     * This will create the window to display the fact & tell the person that they won.
+     * @param 
+     */
+    public BingoFactGUI( String Fact )
+    {
+        WinnerText.setText("Congrats, You Won! Enjoy your BINGO FACT!");
+        FactText.setText(Fact);
+        Frame.setLayout(new FlowLayout());
+        Frame.add(WinnerText);
+        Frame.add(FactText);
+        Frame.add(Ok);
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Frame.pack();
+        Frame.setVisible(true);
+    }
+}
