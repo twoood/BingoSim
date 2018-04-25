ff1c51d311072afe5c50337bd15f2d4d7e427ae0
twoood <31707618+twoood@users.noreply.github.com>
2018-04-25 10:46:54 -0500

    Update BingoFactGUI.java
    Changed BingoFact Display to be more centered and used JLabels instead of JText

--- BingoSim/src/Game/BingoFactGUI.java
+++ BingoSim/src/Game/BingoFactGUI.java
@@ -15,23 +15,38 @@ import javax.swing.*;
 public class BingoFactGUI
 {
     private final JFrame Frame = new JFrame();
-    private final JButton Ok = new JButton("Ok");
-    private final JTextField WinnerText = new JTextField(50);
-    private final JTextField FactText = new JTextField(50);
+    private JLabel WinnerText = new JLabel();
+    private JLabel FactText = new JLabel();
+    
     /**
      * This will create the window to display the fact & tell the person that they won.
      * @param 
      */
     public BingoFactGUI( String Fact )
     {
-        WinnerText.setText("Congrats, You Won! Enjoy your BINGO FACT!");
-        FactText.setText(Fact);
-        Frame.setLayout(new FlowLayout());
-        Frame.add(WinnerText);
-        Frame.add(FactText);
-        Frame.add(Ok);
+        JPanel container = new JPanel();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        WinnerText = new JLabel("Congrats, You Won! Enjoy your BINGO FACT!");
+        WinnerText.setFont(new Font("Serif", Font.BOLD, 20));
+        WinnerText.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        JLabel Blank = new JLabel("\n");
+        JLabel Blank1 = new JLabel("\n");
+        JLabel Blank2 = new JLabel("\n");
+        FactText = new JLabel(Fact);
+        FactText.setFont(new Font("Serif", Font.BOLD, 15));
+        FactText.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        WinnerText.setBorder(BorderFactory.createCompoundBorder(
+                   BorderFactory.createLineBorder(Color.black),
+                   WinnerText.getBorder()));
+        container.add(Blank);
+        container.add(WinnerText);
+        container.add(Blank1);
+        container.add(Blank2);
+        container.add(FactText);
+        Frame.add(container);
         Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Frame.pack();
+        Frame.setSize(1500, 150);
         Frame.setVisible(true);
     }
 }
